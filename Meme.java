127919a61842ef9e51dac5d75f6276301c74eab8
stevenaeola <s.p.bradley@durham.ac.uk>
2017-01-17 16:00:42 +0000

    Added get and set methods

--- Meme.java
+++ Meme.java
@@ -3,18 +3,51 @@ public class Meme
 {
     private boolean isItFunny;
     private String platformOfOrigin;
+    private String text;
     private int year;
     
-    public Meme(String platform, int yearOfCreation)
+    public Meme(String text, String platform, int yearOfCreation)
     {
-        isItFunny = false;
+	this.text = text;
         platformOfOrigin = platform;
         year = yearOfCreation;
+        isItFunny = false;
     }
-    
+
+    public boolean isItFunny(){
+	return isItFunny;
+    }
+
+    public void itIsFunny(){
+	isItFunny = true;
+    }
+
+    public void itIsNotFunny(){
+	isItFunny = false;
+    }
     
     public String getPlatform()
     {
         return platformOfOrigin;
+    }
+
+    public void setPlatform(String platform){
+	platformOfOrigin = platform;
+    }
+
+    public int getYear(){
+	return year;
+    }
+
+    public void setYear(int year){
+	this.year = year;
+    }
+
+    public String getText(){
+	return text;
+    }
+
+    public void setText(String text){
+	this.text = text;
     }
 }
